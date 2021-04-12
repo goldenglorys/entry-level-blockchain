3fcc5f912e7c96a6a1c97cb78cfa7b9cdb959766
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-12 18:12:34 +0100

    Added solidity file for renvm and react interface

--- renProject/src/lib/icons/index.ts
+++
@@ -1,9 +0,0 @@
-import BSC from "./bsc.png";
-import Metamask from "./metamask.png";
-import WalletConnect from "./walletconnect.png";
-
-export const Icons = {
-    BSC,
-    Metamask,
-    WalletConnect,
-};
--- renProject/src/network.ts
+++
@@ -1,9 +0,0 @@
-import {
-    getRenNetworkDetails,
-    RenNetwork,
-    RenNetworkDetails,
-} from "@renproject/interfaces";
-
-export const NETWORK: RenNetworkDetails = getRenNetworkDetails(
-    RenNetwork.TestnetVDot3
-);
